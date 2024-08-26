3be6aaf3ae3b9e22e708240a4cf2d84c4da4f395
0xLPircy <taveesha.agarwal@gmail.com>
2024-08-26 21:26:56 +0530

    unstaking, ao flow error

--- contracts/platform.lua
+++ contracts/platform.lua
@@ -2,31 +2,38 @@ local utils = require(".utils")
 local json = require("json")
 -- local constants = require("helpers.constants")
 local verifiedBridgedToken = require("utils.bridgedTokens")
+local db = require("lsqlite3").open_memory()
 
 AOTOKENID = "abc"
 
-DUMMY SQL TABLE IDK
-USER:
-    userID
-    transactions table (?)
-PROJECT:
-    projectID
-    pTokenID
-    usersStaked
-TRANSACTIONS:
-    {
-        time(?)
-        userID
-        bridgedID
-        Quantity
-        projectID
-    }
-BRIDGEDTOKENS: 
-    AOETH: id
-    AOSOL: id
-TOTALS:
-    shall this be on the pid or bid (?)
-    projectID.bridgedID.Quantity
+db:exec[[CREATE TABLE ]]
+
+-- DUMMY SQL TABLE IDK
+-- USER:
+--     userID
+-- --    transactions table (?)
+-- PROJECT:
+--     projectID
+--     pTokenID
+--     usersStaked
+-- TRANSACTIONS:
+--     {
+--         time(?)
+--         userID
+--         bridgedID
+--         Quantity
+--         projectID
+--         type
+--     }
+-- BRIDGEDTOKENS: 
+--     AOETH: id
+--     AOSOL: id
+-- TOTALS:
+--     shall this be on the pid or bid (?)
+--     projectID.bridgedID.Quantity
+
+
+-- NOTE: cant update totals, what if ao triggering notif, is last 5 min wala calculation and more has come since
 
 
 
@@ -36,17 +43,32 @@ Handlers.add(
     "RegisterUser",
     Handlers.utils.hasMatchingTag("Action", "Register-User"),
     function(msg)
-       
-        local tags = msg.Tags
+        -- local tags = msg.Tags
          -- CHECK USER TABLE, if not then add
         if not Users[msg.From] then Users[msg.From] = "0" end
         -- ADD OTHER INFO
     end
 )
 
--- STAKE BY USER
 Handlers.add(
     "Stake",
+    Handlers.utils.hasMatchingTag("Action", "Stake"),
+    function(msg)
+        -- CHECK USER TABLE, if not then add or send notif to register (?)
+        if not Users[msg.Sender] then Users[msg.Sender] = "0" end 
+        ao.send(
+            Target = msg.Tags.bridgedID,
+            Action = "Transfer",
+            Recipient = msg.From,
+            Quantity = msg.Tags.Quantity,
+            ["X-Action"] = "Stake"
+        )
+    end
+)
+
+-- STAKE BY USER
+Handlers.add(
+    "Staked",
     Handlers.utils.hasMatchingTag("Action", "Credit-Notice") and Handlers.utils.hasMatchingTag("X-Action", "Stake"),
     function(msg)
         -- CHECK USER TABLE, if not then add or send notif to register (?)
@@ -70,6 +92,7 @@ Handlers.add(
                 -- TRANSACTIONS.userID= msg.Sender, 
                 -- TRANSACTIONS.bridgedID = msg.From, 
                 -- TRANSACTIONS.projectID = msg.["X-projectID"]
+                -- TRANSACTION.type = credit
                 -- store to project = X-ProjectID (?)
             -- ADD TO TOTALS
                 -- agar we doing projectID
@@ -143,8 +166,18 @@ Handlers.add(
     Handlers.utils.hasMatchingTag("Action", "Un-Stake"),
     function(msg)
         -- check against existing users
-        -- projectID, amount, bridged token ID
+        if not Users[msg.From] then
+        
+        else
         -- Send back Bridged token
+            ao.send(
+                Target = msg.Tags.bridgedID,
+                Action = "Transfer",
+                Recipient = msg.From,
+                Quantity = msg.Tags.Quantity,
+                ["X-Action"] = "Unstaking"
+            )
+        end
     end
 )
 
@@ -153,7 +186,30 @@ Handlers.add(
     Handlers.utils.hasMatchingTag("Action", "Debit-Notice") and Handlers.utils.hasMatchingTag("x-Action", "Unstaking"),
     function(msg)
         -- check against bridged token IDs
-        -- change User: msg.Recipient, Project: X-ProjectID, Bridged Token: msg.From
+        if not Bridged[msg.From] then
+            ao.send(
+                Target = msg.Sender,
+                Data = "Not a verified Token"
+            )
+            -- SHALL WE BE SENDING SECURITY IMPLEMENTATIONS (?)
+            ao.send(
+                Target = msg.From,
+                Action = "Transfer",
+                Recipient = msg.Sender,
+                Quantity = msg.Quantity
+            )
+        else
+            -- log the info
+                -- TRANSACTIONS.amount= msg.Quantity, 
+                -- TRANSACTIONS.userID= msg.Sender, 
+                -- TRANSACTIONS.bridgedID = msg.From, 
+                -- TRANSACTIONS.projectID = msg.["X-projectID"]
+                -- TRANSACTION.type = credit
+                -- store to project = X-ProjectID (?)
+            -- ADD TO TOTALS
+                -- agar we doing projectID
+                    -- TOTALS.projectID.bridgedID.Quantity
+        end
     end
 )
 
