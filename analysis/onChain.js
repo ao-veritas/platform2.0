8928c21265abdddde2491d8441d9299e986b0125
0xLPircy <taveesha.agarwal@gmail.com>
2024-08-28 12:26:19 +0530

    analysis skeletons

---
+++ analysis/onChain.js
@@ -0,0 +1,25 @@
+const project = {
+    name:"",
+    processID:"",
+    tokenID:"",
+    tokensHeld:[{
+        id:"",
+        ticker:"",
+        quantity:""
+    }],
+    // balances
+    distribution:[{
+        id:"",
+        quantity:""
+    }],
+    // week? day?
+    tokenFlowPerWeek:"",
+    messagesPerDay:"",
+    uniqueUsers:{
+        daily:"",
+        weekly:"",
+        monthly:"",
+        lifetime:""
+    },
+    typesOfMessages:["",]
+}
