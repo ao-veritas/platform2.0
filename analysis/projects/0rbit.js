5ce73bd8dce5730f64a387433fa07461d750f614
0xLPircy <taveesha.agarwal@gmail.com>
2024-08-29 08:31:14 +0530

    off chain info for 0rbit and btteridea

---
+++ analysis/projects/0rbit.js
@@ -0,0 +1,105 @@
+const project = {
+    name: "0rbit",
+    processID:"BaMK1dfayo75s3q1ow6AO64UDpD9SEFbeE8xYrY2fyQ",
+    logoImageLink:"https://www.0rbit.co/logos/ogLogo.jpg",
+    links: {
+        website: "https://0rbit.co/",
+        docs:"https://docs.0rbit.co/",
+        discord: "https://discord.gg/JVSjqaKJgV",
+        twitter: "https://twitter.com/0rbitco",
+        github: "https://github.com/0rbit-co",
+        other: ["https://www.playground.0rbit.co/","https://mirror.xyz/0x26B11B188E9E69b2426FD6111302E721F423020E"]
+    },
+    team: [{
+        officialName:"Yash Garg",
+        pseudoName:"megabyte",
+        role:"Co-Founder",
+        imgLink:"https://0rbit.co/team/megabyte.png",
+        links:{
+            github:"https://github.com/megabyte0x",
+            twitter:"https://x.com/megabyte0x?t=WZYKcJAvN-CM7a6yU4lPNQ&s=09",
+            dribble:"",
+            other:["",""],
+        },
+    },
+    {
+        officialName:"Ayush Agrawal",
+        pseudoName:"lucifer0x17",
+        role:"Co-Founder",
+        imgLink:"https://0rbit.co/team/lucifer.png",
+        links:{
+            github:"https://github.com/Lucifer0x17",
+            twitter:"https://x.com/Lucifer0x17?t=fH5LRms3xy2hSPLJbNubaA&s=09",
+            dribble:"",
+            other:["",""],
+        },
+    },
+    {
+        officialName:"Manishi Bhatnagar",
+        pseudoName:"",
+        role:"UI/ UX Designer",
+        imgLink:"https://0rbit.co/team/manishi.png",
+        links:{
+            github:"",
+            twitter:"https://x.com/0xManishi?t=FKn7XBJwlIXwJR-f4KGkzw&s=09",
+            dribble:"https://dribbble.com/0xManishi",
+            other:["",""],
+        },
+    },
+    {
+        officialName:"Sarthak Shah",
+        pseudoName:"",
+        role:"Engineer",
+        imgLink:"https://0rbit.co/team/sarthak.png",
+        links:{
+            github:"https://github.com/Not-Sarthak",
+            twitter:"https://x.com/0xSarthak13?t=nvsUz9hxhq2hQO25wr8Rtw&s=09",
+            dribble:"",
+            other:["",""],
+        },
+    }],
+    description: "The Decentralized Oracle Network on AO for accessing any off-chain data.",
+    oneLiner: "Decentralized Oracle Network on Arweave",
+    token: {
+        name:"0rbt",
+        ticker:"0RBT",
+        processId:"BUhZLMwQ6yZHguLtJYA5lLUa9LQzLXMXRfaq9FVcPJc",
+        denomination:"",
+        totalSupply:"",
+        tokenomics:{
+            info:"",
+            linkToBlogorPaper:""
+        }
+    },
+    gettingStartedGuide:"",
+    projectOrigin:"",
+    useCases: [{
+        name:"",
+        info:"",
+        liveLink:"",
+        other:[""]
+    }],
+    advisorsInvestors:[{
+        name:"",
+        role:"",
+        moreInfo:"",
+        amountIfAny:[""],
+    }],
+    mileStones:[{
+        goal:"",
+        date:"",
+        proof:"",
+        status:""
+    }],
+    mediaMentions:[""],
+    collaborations:[{
+        name:"",
+        link:"",
+        info:""
+    }],
+    ownershipPercentages:[{
+        name:"",
+        role:"",
+        percentage:""
+    }]
+}
---
+++ analysis/projects/BetterIDEa.js
@@ -0,0 +1,69 @@
+const project = {
+    name: "BetterIDEa",
+    processID:"",
+    logoImageLink:"https://betteridea.dev/_next/static/media/logo.86dc9c02.png",
+    links: {
+        website: "https://betteridea.dev/#home",
+        docs:"https://docs.betteridea.dev/",
+        discord: "https://discord.gg/nm6VKUQBrA",
+        twitter: "https://x.com/betteridea_dev",
+        github: "https://github.com/betteridea-dev",
+        other: ["https://ide.betteridea.dev/","https://mirror.xyz/0xCf673b87aFBed6091617331cC895376209d3b923", "https://linkedin.com/company/betteridea-dev"]
+    },
+    team: [{
+        officialName:"Ankush",
+        pseudoName:"weeblet",
+        role:"Founder",
+        imgLink:"https://pbs.twimg.com/profile_images/1797158678032003072/kD_OsxAF_400x400.jpg",
+        links:{
+            github:"https://github.com/ankushKun",
+            twitter:"https://x.com/ankushKun_",
+            dribble:"",
+            other:["https://linkedin.com/in/ankushKun",""],
+        },
+    }],
+    description: "",
+    oneLiner: "",
+    token: {
+        name:"",
+        ticker:"",
+        processId:"",
+        denomination:"",
+        totalSupply:"",
+        tokenomics:{
+            info:"",
+            linkToBlogorPaper:""
+        }
+    },
+    gettingStartedGuide:"",
+    projectOrigin:"",
+    useCases: [{
+        name:"",
+        info:"",
+        liveLink:"",
+        other:[""]
+    }],
+    advisorsInvestors:[{
+        name:"",
+        role:"",
+        moreInfo:"",
+        amountIfAny:[""],
+    }],
+    mileStones:[{
+        goal:"",
+        date:"",
+        proof:"",
+        status:""
+    }],
+    mediaMentions:[""],
+    collaborations:[{
+        name:"",
+        link:"",
+        info:""
+    }],
+    ownershipPercentages:[{
+        name:"",
+        role:"",
+        percentage:""
+    }]
+}
