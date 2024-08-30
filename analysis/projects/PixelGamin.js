c30bff2611f7d616a3c3a5bddd4807614497ca6f
0xLPircy <taveesha.agarwal@gmail.com>
2024-08-30 08:32:14 +0530

    other projects skeleton for offchain

---
+++ analysis/projects/PixelGamin.js
@@ -0,0 +1,69 @@
+const project = {
+    name: "Pixel Gaming",
+    processID:"",
+    logoImageLink:"",
+    links: {
+        website: "https://www.pixelgaming.us/",
+        docs:"",
+        discord: "",
+        twitter: "",
+        github: "https://github.com/FluXtream-Move",
+        other: ["",""]
+    },
+    team: [{
+        officialName:"",
+        pseudoName:"",
+        role:"",
+        imgLink:"",
+        links:{
+            github:"",
+            twitter:"",
+            dribble:"",
+            other:["",""],
+        },
+    }],
+    description: "",
+    oneLiner: "",
+    token: {
+        name:"",
+        ticker:"",
+        processId:"",
+        denomination:"",
+        totalSupply:"",
+        tokenomics:{
+            info:"",
+            linkToBlogorPaper:""
+        }
+    },
+    gettingStartedGuide:"",
+    projectOrigin:"",
+    useCases: [{
+        name:"",
+        info:"",
+        liveLink:"",
+        other:[""]
+    }],
+    advisorsInvestors:[{
+        name:"",
+        role:"",
+        moreInfo:"",
+        amountIfAny:[""],
+    }],
+    mileStones:[{
+        goal:"",
+        date:"",
+        proof:"",
+        status:""
+    }],
+    mediaMentions:[""],
+    collaborations:[{
+        name:"",
+        link:"",
+        info:""
+    }],
+    ownershipPercentages:[{
+        name:"",
+        role:"",
+        percentage:""
+    }]
+}
